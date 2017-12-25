06ecf66bb391129161ba6827ae8d062ec9318d23
Ahmed Riza <dr.ahmed_riza@yahoo.co.uk>
2017-12-25 15:43:13 +0000

    modify script

--- consolas.sh
+++ consolas.sh
@@ -6,3 +6,4 @@ cd temp
 wget http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe
 cabextract -L -F ppviewer.cab PowerPointViewer.exe
 cabextract ppviewer.cab
+echo "You can now add the fonts to your system"
